37584f0e09d88753cc0c03b6f4dac71b362853fb
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2024-08-19 00:16:22 -0600

    chore: Added Zen Browser logo to Page's Tile

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { pathToRoot } from "../util/path"
+import { joinSegments, pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
@@ -6,8 +6,11 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const iconPath = joinSegments(baseDir, "static/icon.png")
   return (
     <h2 class={classNames(displayClass, "page-title")}>
+      <img src={iconPath} alt="Zen Browser Logo" width="70px" />
+      <br/>
       <a href={baseDir}>{title}</a>
     </h2>
   )
