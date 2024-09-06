a4e393439f202bb9250d9b7bab8f735e46e5c4f2
NOCanoa <158103554+NOCanoa@users.noreply.github.com>
2024-09-06 01:48:59 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,6 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: { provider: 'plausible' },
-    analytics: { provider: 'umami', host: 'https://umami-production-5b46.up.railway.app', websiteId: 'd3957890-d2d5-43fa-9816-0aa259d92353'},
     locale: "en-US",
     baseUrl: "docs.zen-browser.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
